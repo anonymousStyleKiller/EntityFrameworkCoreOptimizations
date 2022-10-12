fbfed018c93496efdb22f2a32cfc5e4c367690c3
Anton <anonymousStyleKiller@gmail.com>
2022-10-12 21:38:01 +0300

    Precompile EF

---
+++ EFCoreOptimizations/Features/Precompile/CatQueries.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreOptimizations.Features.Precompile;
+
+public class CatQueries
+{
+    public static Func<CatsDbContext, int, IEnumerable<CatResult>> CatQuery =>
+        EF.CompileQuery((CatsDbContext db, int age) => 
+        db.Cats.Where(c=>
+            c.BirthDate.Year ==2022&&
+            c.Age < age
+            ).Select(c=>new CatResult
+        {
+            Age = c.Age,
+            Name = c.Name 
+        }));
+}
