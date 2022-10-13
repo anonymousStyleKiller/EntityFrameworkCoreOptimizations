a1c6edc8cc7b605b88d466b258b02908e406885c
Anton <anonymousStyleKiller@gmail.com>
2022-10-13 23:23:16 +0300

    feature(EF): Lesson 8 Change Tracking

---
+++ EFCoreOptimizations/Features/ChangeTracking/Audit.cs
@@ -0,0 +1,12 @@
+﻿namespace EFCoreOptimizations.Features.ChangeTracking;
+
+public class Audit
+{
+    public void ChangeTracking()
+    {
+        using var db = new CatsDbContext();
+        var cat = db.Cats.FirstOrDefault(i=>i.Age == 2022);
+        cat.Name = "New Cat";
+        db.SaveChanges();
+    }
+}
